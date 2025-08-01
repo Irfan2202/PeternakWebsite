4469074b1a23db1b02dc0c9073f018fec8bce5d5
irfan2202 <irfanapurnomo02@gmail.com>
2025-08-01 22:32:05 +0700

    Initial push after refactor

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-Route::middleware(['auth', 'is_admin'])->group(function () {
-    Route::get('/admin', [AdminController::class, 'index']);
-});
+
+Route::get('/login', function () {
+    return redirect('/admin/login');
+})->name('login');
