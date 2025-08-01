be70e8124859192d4dcb423255d563596fa90342
irfan2202 <irfanapurnomo02@gmail.com>
2025-08-01 14:07:14 +0700

    Fix: load CSS & JS directly from Vite build output

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Page Title' }}</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('build/assets/app-DvB2Xm2x.css') }}">
+
     @livewireStyles
 
 </head>
@@ -16,7 +17,7 @@
     {{ $slot }}
     <livewire:footer />
     @livewireScripts
-
+    <script src="{{ asset('build/assets/app-DURVwG0O.js') }}" defer></script>
 </body>
 
 </html>
